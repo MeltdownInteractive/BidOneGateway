cf6630bd133593029d5cfae1a3fef9bf8d5c88a6
Greg Quinn <greg@meltdowninteractive.com>
2025-09-15 14:32:06 +1200

    Added ProductDto model

---
+++ BidOneGateway.Domain/Models/Dto/ProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BidOneGateway.Domain.Models.Dto;
+
+public class ProductDto {
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int StockLevel { get; set; } 
+    public string? NewV2Field { get; set; }
+}
